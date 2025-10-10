69281780455ee56dba2d2315784b36f47215cf77
SerraTak <stak@terpmail.umd.edu>
2025-10-09 20:09:30 -0400

    Update function_library.py

--- SRC/function_library.py
+++ SRC/function_library.py
@@ -3,6 +3,7 @@
 
 
 #simple
+
 def validate_rental_address(address):
    """Validate the rental address format.
     Args:
@@ -38,6 +39,39 @@ def validate_rental_address(address):
    
    return True
 
+def validate_rent_input(price):
+    """Validate the rent input format, need positive number.
+    Args:
+         price (float): The monthly rent entered by the user. 
+
+    Returns:
+         bool: True if the price is valid - positive -, False otherwise.
+
+    Raises:
+         TypeError: If the price is not a float or int.
+         ValueError: If the price is negative, price <= 0.
+
+    Examples:   
+            >>> validate_rent_input(1200.50)
+            True
+            >>> validate_rent_input(-500)
+            Traceback (most recent call last):
+            ...
+            ValueError: Rent price cannot be negative.
+            >>> validate_rent_input("1000")
+            Traceback (most recent call last):
+            ...
+            TypeError: Rent price must be a number.
+    """
+    #Type check
+    if not isinstance(price, (int, float)):
+        raise TypeError("Rent price must be a number.")
+    
+    # Negative check
+    if price <= 0:
+        raise ValueError("Rent price cannot be negative or equal to zero.")
+    
+    return True
 
 
 #medium
