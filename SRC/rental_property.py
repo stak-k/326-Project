048ce38872020ddb04e6ebdb0e496db42fc6f981
bryansturgis <bryansturgis15@gmail.com>
2025-11-02 05:48:08 -0500

    Worked on validating data in the RentalProperty Class

--- SRC/rental_property.py
+++ SRC/rental_property.py
@@ -1,5 +1,8 @@
 # class that stores the rental property data entered by the user
 
+# Import Validator and Coordinates
+from validator import Validator
+from coordinates import Coordinates
 # Import necessary formatters
 from formatter import (
     format_address,
@@ -12,5 +15,24 @@ class RentalProperty:
     def __init__(self, address: str, rent: float, zipcode: int, utilities_included: bool):
         """Initialize a rental property with validation and formatting"""
 
-        # Format each input
-        self.address = format_address(address)
+        # Create helper objects
+        self.validator = Validator()
+        self.coordinates = None  # Will be set after address validation
+
+        # Validate and set address
+        if self._validator.validate_address(address):
+            self._address = format_address(address)
+            # Initialize Coordinates object after address is validated
+            self.coordinates = Coordinates(self._address).coordinates
+
+        
+        # Validate and set rent
+        if self._validator.validate_rent(rent):
+            self._rent = float(rent)
+
+        # Validate and set zipcode
+        if self._validator.validate_zip(zipcode):
+            self._zipcode = int(zipcode)
+
+        # Set utilities included
+        self._utilities_included = bool(utilities_included)
