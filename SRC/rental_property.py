7922a3993f54fcc55b91c5d1bfb29d4a55630943
bryansturgis <bryansturgis15@gmail.com>
2025-11-02 06:10:58 -0500

    Implemented the property section with getters and setters and added a method for summarizing the data for saving or exporting.

--- SRC/rental_property.py
+++ SRC/rental_property.py
@@ -16,14 +16,14 @@ class RentalProperty:
         """Initialize a rental property with validation and formatting"""
 
         # Create helper objects
-        self.validator = Validator()
-        self.coordinates = None  # Will be set after address validation
+        self._validator = Validator()
+        self._coordinates = None  # Will be set after address validation
 
         # Validate and set address
         if self._validator.validate_address(address):
             self._address = format_address(address)
             # Initialize Coordinates object after address is validated
-            self.coordinates = Coordinates(self._address).coordinates
+            self._coordinates = Coordinates(self._address).coordinates
 
         
         # Validate and set rent
@@ -36,3 +36,76 @@ class RentalProperty:
 
         # Set utilities included
         self._utilities_included = bool(utilities_included)
+
+
+
+
+    # Address Getter
+    @property
+    def address(self):
+        """Returns the rental property address"""
+        return self._address
+        
+    # Adress Setter
+    @address.setter
+    def address(self, new_address: str):
+        """Sets the rental property address after validation and formatting"""
+        if self._validator.validate_address(new_address):
+            self._address = format_address(new_address)
+            # Update coordinates when address changes
+            self.coordinates = Coordinates(self._address).coordinates
+
+        
+    # Rent Getter 
+    @property 
+    def rent(self): 
+        """Returns the rent for the rental property"""
+        return self._rent 
+
+    # Rent Setter
+    @rent.setter
+    def rent(self, new_rent: float):
+        """Sets the rent for the rental property after validation"""
+        if self._validator.validate_rent(new_rent):
+            self._rent = float(new_rent)
+            
+    # Zipcode Getter
+    @property
+    def zipcode(self):
+        """Returns the zipcode for the rental property"""
+        return self._zipcode
+    
+    # Zipcode Setter
+    @zipcode.setter
+    def zipcode(self, new_zipcode: int):
+        """Sets the zipcode for the rental property after validation"""
+        if self._validator.validate_zip(new_zipcode):
+            self._zipcode = int(new_zipcode) 
+            
+    # Utilities Included Getter 
+    @property 
+    def utilities_included(self):
+         """Returns whether utilities are included in the rent"""
+         return self._utilities_included
+    
+    # Utilities Included Setter
+    @utilities_included.setter
+    def utilities_included(self, included: bool):
+         """Sets whether utilities are included in the rent"""
+         self._utilities_included = bool(included)
+         
+    # Coordinates Getter
+    @property 
+    def coordinates(self):
+        """Returns the (latitude, longitude) tuple for the rental property"""
+        return self._coordinates
+    
+    # Summary Method for saving or exporting data 
+    def summary(self):
+         """Returns a summary representation of the property"""
+         return{ "address": self._address,
+            "rent": self._rent,
+            "zipcode": self._zipcode,
+            "utilities_included": self._utilities_included,
+            "coordinates": self._coordinates
+            }                 
