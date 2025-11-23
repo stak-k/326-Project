3df66d07ebabb61c4ab0073fd45eccc1686cf4fe
SerraTak <stak@terpmail.umd.edu>
2025-11-23 14:55:38 -0500

    Update property_type.py
    Room & Shared House options added

--- SRC/property_type.py
+++ SRC/property_type.py
@@ -6,3 +6,17 @@ class Studio(Property):
     
     def type_score(self):
         return 9
+
+class Room(Property):
+    def rental_type(self):
+        return "Room"
+    
+    def type_score(self):
+        return 6.5
+
+class SharedHouse(Property)
+    def rental_type(self):
+        return "Shared House"
+    
+    def type_score(self):
+        return 7
