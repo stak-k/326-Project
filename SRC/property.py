8477b2f5d4fefb00a0351531c122ca941493d45e
SerraTak <stak@terpmail.umd.edu>
2025-11-23 14:40:55 -0500

    Create abc and parent class
    + child file name changed  to " property_type"

--- SRC/property.py
+++ SRC/property.py
@@ -1,1 +1,20 @@
+from abc import ABC, abstractmethod
+from formatter import format_address
+
+
+class Property(ABC):
+    def __init__(self, address: str, price: float, lease_term: str):
+        self.address = address
+        self.price = price
+        self.lease_term = lease_term
+
+    @abstractmethod
+    def rental_type(self) -> str:
+        """Return property type name( 'Studio', 'Room')"""
+        pass
+
+    @abstractmethod
+    def type_score(self) -> float:
+        """Return score based on property type"""
+        pass
 
